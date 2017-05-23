152f407273224275a94264f28918f90d12a55cec
Panos Louridas <louridas@gmail.com>
2017-05-23 22:37:05 +0300

    use argparse and show number of words

--- eu_gensim/eu_lda_gensim.py
+++ eu_gensim/eu_lda_gensim.py
@@ -18,16 +18,25 @@ from time import time
 import string
 import logging
 
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('dataset',
+                    action='store',
+                    choices=['FP4', 'FP5', 'FP6', 'FP7', 'H2020'],
+                    help='input dataset')
 
-df_name = ''
-while df_name not in ['FP4', 'FP5', 'FP6', 'FP7', 'H2020']:
-    df_name =raw_input("EU data: Choose dataset (options: FP4, FP5, FP6, FP7, H2020): ")
+args = parser.parse_args()
+
+df_name = args.dataset
 
 df = pd.read_pickle('dfs/' + df_name)
 df1 = df[['title','objective']]
 df1 = df1.dropna(how='any')
 df1['merged'] = df1['title'] + ' ' + df1['objective']
+
+num_words = df1['merged'].str.lower().str.split().apply(lambda x: len(x)).sum()
+print(num_words, ' read')
 
 objectives = df1['merged']
 
@@ -50,16 +59,28 @@ objectives_split = objectives_split.apply(lambda tokens: [token for token in tok
 
 objectives_dictionary = Dictionary(objectives_split)
 
-class ObjectivesCorpus(object):
-    def __init__(self, documents, dictionary):
-        self.documents = documents
-        self.dictionary = dictionary
-    def __iter__(self):
-        for document in self.documents:
-            yield self.dictionary.doc2bow(document)
+class ObjectivesCorpus(corpora.textcorpus.TextCorpus):
+    def get_texts(self):
+        return iter(self.input)
+    def __len__(self):
+        """Define this so we can use `len(corpus)`"""
+        if 'length' not in self.__dict__:
+            logging.info('caching corpus size '
+                         '(calculating number of documents)')
+            self.length = sum(1 for doc in self.get_texts())            
+        return self.length
+    def __str__(self):
+        if 'num_words' not in self.__dict__:
+            logging.info('caching corpus size '
+                         '(calculating number of words)')
+            self.num_words = sum(len(doc) for doc in self.get_texts())
+        return (str(self.__len__()) + ' documents, ' + str(self.num_words)
+                + ' words')
+            
 
-objectives_corpus = ObjectivesCorpus(objectives_split, objectives_dictionary)
+objectives_corpus = ObjectivesCorpus(objectives_split)
 
+print(objectives_corpus)
 
 t0 = time()
 iterations = 300
@@ -68,13 +89,13 @@ if df_name == 'FP7':
 elif df_name == 'H2020':
     iterations = 250
 lda = gensim.models.ldamodel.LdaModel(corpus=objectives_corpus, 
-                                              id2word=objectives_dictionary, 
-                                              num_topics=10,
-                                              iterations = iterations,
-                                              random_state=np.random.seed(42))
+                                      id2word=objectives_dictionary, 
+                                      num_topics=10,
+                                      iterations = iterations,
+                                      random_state=np.random.seed(42))
 print("done in %0.3fs." % (time() - t0))
 
-# print lda.print_topics(num_topics=10, num_words=15)
+lda.print_topics(10)
 
 for t in range(lda.num_topics):
     words = dict(lda.show_topic(t, 15))
